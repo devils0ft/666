91a923f420315e5c4f90913df83ec33fc5c2af64
devils0ft <transhvh@gmail.com>
2024-11-20 14:21:55 -0800

    Update script.js

--- script.js
+++ script.js
@@ -9,3 +9,28 @@ document.addEventListener('DOMContentLoaded', function() {
         }, 2000); // 2000 миллисекунд = 2 секунды
     });
 });
+
+
+document.getElementById('rotateButton').addEventListener('click', function() {
+    // Сначала случайным образом выберем одну картинку
+    const images = document.querySelectorAll('.image');
+    const randomIndex = Math.floor(Math.random() * images.length);
+    const selectedImage = images[randomIndex];
+
+    // Вращаем изображения
+    images.forEach((image, index) => {
+        image.style.transform = 'rotate(360deg)';
+    });
+
+    // В течение 4 секунд
+    setTimeout(() => {
+        // Возвращаем картинки в исходное положение
+        images.forEach((image) => {
+            image.style.transform = 'rotate(0deg)';
+        });
+
+        // Сделаем одну картинку немного выше, оставив её на месте
+        selectedImage.style.top = '-30px';  // Сдвигаем выбранную картинку
+    }, 4000); // 4 секунды
+
+});
